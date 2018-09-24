0ab8b8804d7fd6de857ff8a7d3bf3302523433e7
iliutaadrian <iliuta_rocky@yahoo.com>
2018-09-24 20:32:03 +0300

    fix update user

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -29,12 +29,35 @@ class ImageController extends Controller
         }
     }
 
-    public function getPreviousAvatars($id){
-        $response = Storage::disk('public')->files('/avatar');
-        $pattern = '/avatar_' . $id . '_.*/';
+    public function changeAvatar(Request $request){
+        if($request->avatar != null){
+            User::where('id', $request->id)->update(['avatar' => $request->avatar ]);
+            return response('avatar updated succesfully', 200);
+        }
+        else{
+            return response('not an image', 400);
+        }
+    }
 
+    public function getPreviousAvatars($id){
+        $currentAvatar = User::where('id', $id)->first()['avatar'];
+        $all = Storage::disk('public')->files('/avatar');
+        $pattern = '/avatar_' . $id . '_.*/';
+        $files = preg_grep($pattern , $all);
+        $response = array_filter($files, function($val) use ($currentAvatar) { if('/storage/' . $val != $currentAvatar) return $val; });
         return response()->json([
-            'pictures' => preg_grep($pattern , $response)
+            'pictures' => $response
         ], 200);
+    }
+
+    public function deletePreviousAvatar(Request $request){
+        $exists = Storage::disk('public')->exists($request->name);
+        if($exists){
+            Storage::disk('public')->delete($request->name);
+            return response('file deleted succesfully', 200);
+        }
+        else{
+            return response('file does not exist', 400);
+        }
     }
 }
--- resources/assets/js/components/user/UserAvatar.vue
+++ resources/assets/js/components/user/UserAvatar.vue
@@ -2,31 +2,54 @@
     <div class="container">
         <p>Upload or select an old avatar</p>
 
-        <vue-avatar
-                :width=150
-                :height=150
-                :border=10
-                ref="vueavatar"
-                @vue-avatar-editor:image-ready="onImageReady"
-        >
-        </vue-avatar>
-        <vue-avatar-scale
-                ref="vueavatarscale"
-                @vue-avatar-editor-scale:change-scale="onChangeScale"
-                :width=170
-                :min=1
-                :max=3
-                :step=0.02
-        >
-        </vue-avatar-scale>
+        <button class="btn btn-info" @click="oldAvatars = !oldAvatars">
+            <span v-if="!oldAvatars">Old avatars</span>
+            <span v-if="oldAvatars">Upload</span>
+        </button>
 
-        <div class="images">
-            <img v-for="image in images" :src="`/storage/${image}`" class="image" @click="updatePreview(image)">
+        <div class="row">
+            <div class="col-md-4" v-if="!oldAvatars">
+                <vue-avatar
+                        :width=150
+                        :height=150
+                        :border=10
+                        ref="vueavatar"
+                        @vue-avatar-editor:image-ready="onImageReady"
+                        :image="preview"
+                >
+                </vue-avatar>
+                <vue-avatar-scale
+                        ref="vueavatarscale"
+                        @vue-avatar-editor-scale:change-scale="onChangeScale"
+                        :width=170
+                        :min=1
+                        :max=3
+                        :step=0.02
+                >
+                </vue-avatar-scale>
+            </div>
+            <div class="col-md-4 images" v-if="oldAvatars">
+                <div v-for="image in images">
+                    <i class="fa fa-trash deleteAvatar" @click="deleteOldAvatar(image)"></i>
+                    <img :src="`/storage/${image}`" class="image" @click="selectOldAvatar(image)">
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <img :src="preview" class="avatarBig">
+            </div>
+            <div class="col-md-4">
+                <img :src="preview" class="avatarSmall">
+            </div>
         </div>
 
-        <br>
-        <button class="btn btn-info" @click="saveClicked">Change Picture</button>
-        <button class="btn btn-info" @click="done">Cancel</button>
+        <div class="row">
+            <div class="col-md-12">
+                <button class="btn btn-info" @click="saveClicked">Change Picture</button>
+                <button class="btn btn-danger" @click="done">Cancel</button>
+            </div>
+        </div>
+
         <br>
     </div>
 
@@ -44,7 +67,9 @@
         data(){
             return{
                 images: [],
-                preview: ''
+                preview: '/storage/avatar/avatar-upload.png',
+                avatar: '',
+                oldAvatars: 0
             }
         },
         created(){
@@ -55,38 +80,60 @@
         },
         methods:{
             onChangeScale (scale) {
-                this.$refs.vueavatar.changeScale(scale)
+                this.$refs.vueavatar.changeScale(scale);
+                this.preview = this.$refs.vueavatar.getImageScaled().toDataURL();
             },
             saveClicked(){
-                var img = this.$refs.vueavatar.getImageScaled();
-                const formData = new FormData();
+                if(this.preview != '/storage/avatar/avatar-upload.png') {
+                    if(this.oldAvatars){
+                        axios.post('/api/user/avatar/change', {id: this.$store.getters.currentUser.id, avatar:this.preview})
+                            .then(res => {
+                                this.$store.getters.currentUser.avatar = this.preview;
+                                this.$store.commit('updateUser', this.$store.getters.currentUser);
 
-                formData.append('image', img.toDataURL());
-                formData.append('id', this.$store.getters.currentUser.id);
+                                this.$emit('doneAvatar');
+                            })
+                            .catch(err => {
 
-                axios.post('/api/user/avatar', formData)
-                    .then(res=>{
-                        this.$store.getters.currentUser.avatar = res.data.path;
-                        this.$store.commit('updateUser', this.$store.getters.currentUser);
+                            })
+                    }
+                    else{
+                        const formData = new FormData();
 
-                        this.$emit('doneAvatar');
-                    })
-                    .catch(err=>{
+                        formData.append('image', this.$refs.vueavatar.getImageScaled().toDataURL());
+                        formData.append('id', this.$store.getters.currentUser.id);
 
-                    })
+                        axios.post('/api/user/avatar', formData)
+                            .then(res => {
+                                this.$store.getters.currentUser.avatar = res.data.path;
+                                this.$store.commit('updateUser', this.$store.getters.currentUser);
 
+                                this.$emit('doneAvatar');
+                            })
+                            .catch(err => {
+
+                            })
+                    }
+                }
             },
             onImageReady(scale){
-                this.$refs.vueavatarscale.setScale(scale)
+                this.$refs.vueavatarscale.setScale(scale);
+                this.preview = this.$refs.vueavatar.getImageScaled().toDataURL();
             },
             done(){
                 this.$emit('doneAvatar');
             },
-            updatePreview(image){
-                this.$store.getters.currentUser.avatar = '/storage/' + image;
-                this.$store.commit('updateUser', this.$store.getters.currentUser);
-
-                this.$emit('doneAvatar');
+            selectOldAvatar(image){
+                this.preview = '/storage/' + image;
+            },
+            deleteOldAvatar(image){
+                axios.post('/api/user/pictures/delete/', {name: image})
+                    .then(res=>{
+                        console.log(res);
+                    })
+                    .catch(err=>{
+                        console.log(err);
+                    })
             }
         }
     }
@@ -94,15 +141,34 @@
 
 <style scoped>
     .images{
-        height: 160px;
+        height: 158px;
         overflow-y: scroll;
         float: left;
         overflow-x: hidden;
     }
 
     .image{
-        margin: 7px;
+        margin: 3px;
         border: 1px solid black;
         cursor: pointer;
+        width: 120px;
      }
+
+    .avatarBig{
+        border: 1px solid black;
+        width: 150px;
+    }
+
+    .avatarSmall{
+        width: 60px;
+        border-radius: 50%;
+    }
+
+    .deleteAvatar{
+        position: relative;
+        top: 34px;
+        left: 100px;
+        cursor: pointer;
+        font-size: 20px;
+    }
 </style>
